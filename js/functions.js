9da3db6bbbc574de81040e14409d22fded715143
Mario Weiss <mobil@ing-weiss.com>
2018-10-24 01:43:33 +0200

    start merging content to template

--- js/functions.js
+++ js/functions.js
@@ -3,14 +3,14 @@ var images = []
 var x = 3; //number of images
 for (let i = 1; i <= x; ++i) {
     images[i] = new Image()
-    images[i].src = "img/location/code" + i + ".jpg";
+    images[i].src = "img/course_space/code" + i + ".jpg";
 }
 // caches images, avoiding white flash between background replacements
 function fadeImage(i) {
 	if (i > x) {i = 1};
-    document.getElementById("location-img").style.background = "url(img/location/code" + i + ".jpg) no-repeat center";
-    document.getElementById("location-img").style.backgroundSize = "cover";
-    document.getElementById("location-img").style.backgroundAttachment = "fixed";
+    document.getElementById("course_space_img").style.background = "url(img/course_space/code" + i + ".jpg) no-repeat center";
+    document.getElementById("course_space_img").style.backgroundSize = "cover";
+    document.getElementById("course_space_img").style.backgroundAttachment = "fixed";
     setTimeout(function () { fadeImage(i+1); }, 8000);
 }
 fadeImage(1);
